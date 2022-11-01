054d70ed3ae7455991fc50186840e0d64500ed79
Abraham_Kol <Abrahamkolawole87@gmail.com>
2022-11-01 12:01:13 +0100

    plat-264 add updates to candidate portal

--- src/data/ethnicityOptions.jsx
+++ src/data/ethnicityOptions.jsx
@@ -1,33 +1,28 @@
 /* eslint-disable import/prefer-default-export */
 export const ethnicityOptions = [
+  { label: 'African', value: 'African' },
   {
-    label: 'Black / African / Caribbean / Black British',
-    value: 'Black / African / Caribbean / Black British',
+    label: 'Any other Asian background',
+    value: 'Any other Asian background',
+  },
+  {
+    label: 'Any other Black, Black British, or Caribbean background',
+    value: 'Any other Black, Black British, or Caribbean background',
+  },
+  {
+    label: 'Any other Mixed or multiple ethnic background',
+    value: 'Any other Mixed or multiple ethnic background',
   },
   { label: 'Arab', value: 'Arab' },
-  { label: 'Asian / Asian British', value: 'Asian / Asian British' },
   { label: 'Bangladeshi', value: 'Bangladeshi' },
   { label: 'Caribbean', value: 'Caribbean' },
   { label: 'Chinese', value: 'Chinese' },
-  {
-    label: 'English / Welsh / Scottish / Northern Irish / British',
-    value: 'English / Welsh / Scottish / Northern Irish / British',
-  },
   { label: 'Indian', value: 'Indian' },
-  { label: 'Irish', value: 'Irish' },
-  {
-    label: 'Mixed - White and Black Caribbean',
-    value: 'Mixed - White and Black Caribbean',
-  },
-  {
-    label: 'Mixed - White and Black African',
-    value: 'Mixed - White and Black African',
-  },
-  { label: 'Mixed - White and Asian', value: 'Mixed - White and Asian' },
-  {
-    label: 'Mixed - Black and Other (please specify in other section)',
-    value: 'Mixed - Black and Other (please specify in other section)',
-  },
   { label: 'Pakistani', value: 'Pakistani' },
-  { label: 'Other', value: 'Other' },
+  { label: 'White and Black Caribbean', value: 'White and Black Caribbean' },
+  {
+    label: 'White and Black African',
+    value: 'White and Black African',
+  },
+  { label: 'White and Asian', value: 'White and Asian' },
 ];
