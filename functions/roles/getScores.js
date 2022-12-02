2e921f0c3abe16c5a58c385b55742cf3787bc612
developerdesigner18 <developer.designer18@gmail.com>
2022-12-02 18:25:05 +0530

    updating matchRoles, getScores fns

--- functions/roles/getScores.js
+++ functions/roles/getScores.js
@@ -1,11 +1,35 @@
-const getScores = (data) => {
-  const { candidateTags, companyTags } = data;
-  if (companyTags && candidateTags) {
-    const intersection = companyTags.filter((x) => candidateTags.includes(x));
-    return Math.round((intersection.length * 100) / companyTags.length);
-  }
-  // remove the Match.random return when algo is fixed
-  return Math.floor(Math.random() * 99);
-  // return 0
+/**
+ *
+ * @param {Object} candidate
+ * @param {Object} role
+ * @returns {Number} score
+ */
+const getScores = (candidate, role) => {
+  const behaviorAttributesScore = calculateTagScore(
+    candidate.behaviorAttributes,
+    role.behaviourAttributesStrengths
+  );
+
+  const technicalSkillsScore = calculateTagScore(
+    candidate.technicalSkills,
+    role.technicalSkills
+  );
+
+  const jobValuesScore = calculateTagScore(candidate.jobValues, role.jobValues);
+
+  const score =
+    (behaviorAttributesScore + technicalSkillsScore + jobValuesScore) / 3;
+
+  return score;
 };
+
+const calculateTagScore = (candidateTags, roleTags) => {
+  if (!Array.isArray(candidateTags) || !Array.isArray(roleTags)) return 0;
+  if (!candidateTags.length || !roleTags.length) return 0;
+
+  const intersection = roleTags.filter((x) => candidateTags.includes(x));
+
+  return Math.round((intersection.length * 100) / roleTags.length);
+};
+
 exports.getScores = getScores;
--- functions/roles/matchRoles.js
+++ functions/roles/matchRoles.js
@@ -2,12 +2,18 @@ const { getScores } = require('./getScores');
 const { addRoleInFirestore } = require('./addMatchedRoleInUserDoc');
 const { getRolesFromFiretore } = require('./getRolesFromFiretore');
 
-const matchRoles = async (uid) => {
+/**
+ *
+ * @param {String} roleUid
+ * @param {Object} candidate
+ */
+const matchRoles = async (roleUid, candidate) => {
   const roles = await getRolesFromFiretore();
+
   if (roles) {
     roles.forEach((role) => {
-      role.score = getScores(role);
-      addRoleInFirestore(role, uid);
+      role.score = getScores(role, candidate);
+      addRoleInFirestore(role, roleUid);
     });
   }
 };
