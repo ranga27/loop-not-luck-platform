e1447a11ac0b9095d670920109d6e0fafd793d30
developerdesigner18 <developer.designer18@gmail.com>
2023-01-10 15:10:54 +0530

    Add Feat: Send email when user matched role with higher than 75%

--- functions/user/index.js
+++ functions/user/index.js
@@ -1,7 +1,9 @@
 const confirmEmail = require('./confirmEmail');
 const sendVerificationEmail = require('./sendVerificationEmail');
 const { sendWebsiteFeedback } = require('./sendWebsiteFeedback');
+const { sendTopMatchedRoleEmail } = require('./sendTopMatchedRoleEmail');
 
 exports.confirmEmail = confirmEmail.confirmEmail;
 exports.sendVerificationEmail = sendVerificationEmail.sendVerificationEmail;
 exports.sendWebsiteFeedback = sendWebsiteFeedback;
+exports.sendTopMatchedRoleEmail = sendTopMatchedRoleEmail;
