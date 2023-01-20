b41e29f4036d50d89f7a767ce8bf7447cd2f4945
developerdesigner18 <developer.designer18@gmail.com>
2023-01-20 17:58:22 +0530

    Fix: Company Verification mail Sending and Conforming

--- functions/user/index.js
+++ functions/user/index.js
@@ -3,9 +3,15 @@ const sendVerificationEmail = require('./sendVerificationEmail');
 const { sendWebsiteFeedback } = require('./sendWebsiteFeedback');
 const { sendTopMatchedRoleEmail } = require('./sendTopMatchedRoleEmail');
 const { sendSaperateEmail } = require('./sendSaperateEmail');
+const {
+  sendCompanyVerificationEmail,
+} = require('./sendCompanyVerificationEmail');
+const { companyEmailConformation } = require('./companyEmailConformation');
 
 exports.confirmEmail = confirmEmail.confirmEmail;
 exports.sendVerificationEmail = sendVerificationEmail.sendVerificationEmail;
 exports.sendWebsiteFeedback = sendWebsiteFeedback;
 exports.sendTopMatchedRoleEmail = sendTopMatchedRoleEmail;
 exports.sendSaperateEmail = sendSaperateEmail;
+exports.sendCompanyVerificationEmail = sendCompanyVerificationEmail;
+exports.companyEmailConformation = companyEmailConformation;
