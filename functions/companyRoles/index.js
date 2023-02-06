16212a106a4c273b4e65f16909ccdb133a20d514
developerdesigner18 <developer.designer18@gmail.com>
2023-02-06 16:47:50 +0530

    Remove companyRoles folder

--- functions/companyRoles/index.js
+++
@@ -1,9 +0,0 @@
-const onUserProfileCompleteV2 = require('./onUserProfileComplete');
-const onUserProfileUpdateV2 = require('./onUserProfileUpdate');
-const onRefreshV2 = require('./onRefresh');
-const onWriteV2 = require('./onWrite');
-
-exports.onUserProfileCompleteV2 = onUserProfileCompleteV2.onUserProfileCompleteV2;
-exports.onUserProfileUpdateV2 = onUserProfileUpdateV2.onUserProfileUpdateV2;
-exports.onRefreshV2 = onRefreshV2.onRefreshV2;
-exports.onWriteV2 = onWriteV2.onWriteV2;
